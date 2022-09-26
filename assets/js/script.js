887f29beb17d9da920f12d11724c2840dae9ee72
JonathanFerreira <jonathanf439@gmail.com>
2022-09-25 22:55:35 -0300

    IMC first

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+
+function calcular(event) {
+	event.preventDefault();
+
+let nomeInput = document.getElementById('nome').value;
+let alturaInput = document.getElementById('altura').value;
+let pesoInput = document.getElementById('peso').value;
+let resultado = document.getElementById('content-result');
+let boxResult = document.getElementById('resultado')
+let tableResult = document.getElementById('table')
+
+let imc = (pesoInput / (alturaInput * alturaInput));
+
+
+	if (imc < 18.5) {
+		resultado.innerHTML = `òla ${nomeInput}, seu resultado foi: ${imc.toFixed(2)} você está na classificação IMC:  MAGREZA`;
+	}else if(imc < 24.9){
+		resultado.innerHTML = `Óla ${nomeInput}, seu resultado foi: ${imc.toFixed(2)} você está na classificação IMC:  NORMAL`;
+	}else if (imc < 29.9){
+		resultado.innerHTML = `Óla ${nomeInput}, seu resultado foi: ${imc.toFixed(2)} você está na classificação IMC:  SOBREPESO`;
+	}else if (imc < 39.9){
+		resultado.innerHTML = `Óla ${nomeInput}, seu resultado foi: ${imc.toFixed(2)} você está na classificação IMC:  OBESIDADE`;
+	}else if (imc > 39.9){
+		resultado.innerHTML = `Óla ${nomeInput}, seu resultado foi: ${imc.toFixed(2)} você está na classificação IMC:  OBESIDADE GRAVE`;
+	}
+
+	resultado.classList.remove('hide')
+	table.classList.remove('hide')
+}
